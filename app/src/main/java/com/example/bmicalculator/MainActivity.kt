25ab3556e6ff3e0b28d5de2037bb4ecae7b583ca
pramod231 <conniviatech333@gmail.com>
2022-09-10 20:01:01 +0530

    latest

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etWeight: EditText
+    private lateinit var etHeight: EditText
+    private lateinit var calculateBtn: Button
+    private lateinit var tvResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etWeight = findViewById(R.id.etWeight)
+        etHeight = findViewById(R.id.etHeight)
+        calculateBtn = findViewById(R.id.calculateBtn)
+        tvResult = findViewById(R.id.tvResult)
+
+
+        calculateBtn.setOnClickListener{
+            getBmi()
+        }
+    }
+
+
+    fun getBmi(){
+
     }
 }
